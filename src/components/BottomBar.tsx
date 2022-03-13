7388856137f65a83640f8d23a7ccc3e2000cacb3
Folded-Code <thomas.reilly.forbes@gmail.com>
2022-03-13 14:17:42 +0000

    added by at bottom

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,12 +1,15 @@
-import { AppBar, Link, Toolbar, Typography } from '@mui/material'
+import { AppBar, Link, Toolbar, Typography, Stack } from '@mui/material'
 
 export default function BottomBar() {
   return (
     <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
       <Toolbar>
-        <Link href="https://forms.gle/geere8U9kzPuWCTH8">
-          <Typography variant="body2">Report Bugs</Typography>
-        </Link>
+        <Stack spacing={2} direction="row">
+          <Typography variant="body2">Built by Thomas Forbes</Typography>
+          <Link href="https://forms.gle/geere8U9kzPuWCTH8">
+            <Typography variant="body2">Report Bugs</Typography>
+          </Link>
+        </Stack>
       </Toolbar>
     </AppBar>
   )
