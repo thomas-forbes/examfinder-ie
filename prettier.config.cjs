fac6feed68373217327cf4b31d49d0ab7f8d9570
Folded-Code <thomas.reilly.forbes@gmail.com>
2022-12-21 14:32:50 +0000

    moved to ct3a

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+};
