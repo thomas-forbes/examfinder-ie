72d7d388151c2c6dbe15793a694cf5d30911f68d
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2023-04-23 12:00:49 +0100

    update social image version

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -3,6 +3,7 @@ export default function PageHead() {
   const description =
     'The best exam material archive of Irish past papers and marking schemes'
   const title = 'Exam Finder: Irish Past Papers'
+  const image = 'https://examfinder.ie/social-image.png?v=2'
   return (
     <Head>
       <title>{title}</title>
@@ -21,14 +22,8 @@ export default function PageHead() {
       <meta name="twitter:creator" content="@thomasforbesy" />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta
-        name="twitter:image"
-        content="https://examfinder.ie/social-image.png"
-      />
-      <meta
-        property="og:image"
-        content="https://examfinder.ie/social-image.png"
-      />
+      <meta name="twitter:image" content={image} />
+      <meta property="og:image" content={image} />
     </Head>
   )
 }
