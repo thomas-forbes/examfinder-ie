9195b01c7e6a3e8bb0bdb5c88eef68c25e74b964
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2023-04-24 10:21:42 +0100

    improvements

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,6 +1,6 @@
 import { Combobox, Transition } from '@headlessui/react'
 import { ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment, useEffect, useRef, useState } from 'react'
 
 interface props {
   value: string
@@ -15,11 +15,20 @@ export default function Autocomplete({
   options,
   renderOption,
 }: props) {
+  const [filteredOpts, setFilteredOpts] = useState(options)
   const [query, setQuery] = useState(value)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     setQuery(value)
   }, [value])
+
+  useEffect(() => {
+    setFilteredOpts(
+      options.filter((opt) => opt.toLowerCase().includes(query.toLowerCase()))
+    )
+  }, [options, query])
+
   return (
     <>
       <Combobox value={value} onChange={onChange}>
@@ -29,6 +38,8 @@ export default function Autocomplete({
               className="h-full w-full rounded-md border-none bg-zinc-900 py-2 pl-3 pr-10 text-lg font-bold focus:ring-0 focus-visible:outline-none sm:text-base"
               onChange={(event) => setQuery(event.target.value)}
               displayValue={() => value}
+              onClick={() => inputRef.current?.select()}
+              ref={inputRef}
             />
             <div className="absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
@@ -45,20 +56,22 @@ export default function Autocomplete({
             afterLeave={() => setQuery('')}
           >
             <Combobox.Options className="absolute z-10 mt-2 max-h-60 w-full overflow-auto rounded-md border border-zinc-200/20 bg-zinc-900 text-base shadow-lg duration-300 focus:outline-none sm:text-sm">
-              {(query.trim() && query !== value
-                ? options.filter((opt) => {
-                    return opt.toLowerCase().includes(query.toLowerCase())
-                  })
-                : options
-              ).map((opt, idx) => (
-                <Combobox.Option
-                  key={opt + idx}
-                  value={opt}
-                  className={`relative cursor-pointer select-none px-4 py-2 text-zinc-400 duration-300 hover:bg-zinc-800 ui-selected:bg-zinc-700 ui-selected:text-white`}
-                >
-                  {renderOption ? renderOption(opt) : opt}
-                </Combobox.Option>
-              ))}
+              {query.trim() && query !== value && filteredOpts.length === 0 && (
+                <p className="px-4 py-2 text-zinc-400">No results found</p>
+              )}
+              {(query.trim() && query !== value ? filteredOpts : options).map(
+                (opt, idx) => (
+                  <Combobox.Option
+                    key={opt + idx}
+                    value={opt}
+                    className={`relative cursor-pointer select-none px-4 py-2 text-zinc-400 duration-300 hover:bg-zinc-800 ui-selected:bg-zinc-700 ui-selected:text-white ${
+                      idx == 0 ? 'bg-zinc-800' : ''
+                    }`}
+                  >
+                    {renderOption ? renderOption(opt) : opt}
+                  </Combobox.Option>
+                )
+              )}
             </Combobox.Options>
           </Transition>
         </div>
