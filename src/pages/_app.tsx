a3a3faa775eb2a1c11540595aa1dd1e29ebe580a
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2025-09-30 10:43:24 +0100

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import { useEffect } from 'react'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
+import { useEffect } from 'react'
 
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: '/ingest',
       ui_host: 'https://us.posthog.com',
       defaults: '2025-05-24',
